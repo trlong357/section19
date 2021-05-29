d0ebc58abbf41bfbcaed293e4e22bc5186d1df22
Long Tran <trlong357@gmail.com>
2021-05-29 22:11:23 +0700

    260. Finalizing the Fetching logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
+import { Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect, Fragment } from "react";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-// import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
-import { sendCartData, fetchCartData } from "./store/cart-action";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 
 let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
-
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
@@ -27,8 +25,11 @@ function App() {
       return;
     }
 
-    dispatch(sendCartData(cart));
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
+
   return (
     <Fragment>
       {notification && (
--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -9,8 +9,9 @@ export const fetchCartData = () => {
       );
 
       if (!response.ok) {
-        throw new Error("Couldn't fetch data");
+        throw new Error("Could not fetch cart data!");
       }
+
       const data = await response.json();
 
       return data;
@@ -18,13 +19,18 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
-          status: "Error",
-          title: "Error",
-          message: "Fetching cart data failed",
+          status: "error",
+          title: "Error!",
+          message: "Fetching cart data failed!",
         })
       );
     }
@@ -36,38 +42,44 @@ export const sendCartData = (cart) => {
     dispatch(
       uiActions.showNotification({
         status: "pending",
-        title: "Sending",
-        message: "Sending cart data",
+        title: "Sending...",
+        message: "Sending cart data!",
       })
     );
+
     const sendRequest = async () => {
       const response = await fetch(
         "https://book-edba5-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
+
       if (!response.ok) {
-        throw new Error("Sending data failed!");
+        throw new Error("Sending cart data failed.");
       }
     };
+
     try {
       await sendRequest();
 
       dispatch(
         uiActions.showNotification({
           status: "success",
-          title: "Sended",
-          message: "Sended cart data",
+          title: "Success!",
+          message: "Sent cart data successfully!",
         })
       );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
-          status: "Error",
-          title: "Error",
-          message: "Sending cart data failed",
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
         })
       );
     }
