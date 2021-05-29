373b3aa032a7853570cb914a21002f81ddc0a338
Long Tran <trlong357@gmail.com>
2021-05-29 14:56:29 +0700

    258. Using an Aciton Creator Thunk

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { useEffect, Fragment } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
+// import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -17,50 +18,13 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending",
-          message: "Sending cart data",
-        })
-      );
-      const response = await fetch(
-        "https://book-edba5-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Sending data failed!");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Sended",
-          message: "Sended cart data",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "Error",
-          title: "Error",
-          message: "Sendeing cart data failed",
-        })
-      );
-    });
-  }, [cart, dispatch]); //re-executes when cart changes
-
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
   return (
     <Fragment>
       {notification && (
